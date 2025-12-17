28df58d73400fad4733c0e4510fec00526b50e81
George Silvas <neogeojr@aol.com>
2025-12-16 18:23:21 -0700

    notes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function playSound(keyCodeOrEvent) {
       ? keyCodeOrEvent
       : keyCodeOrEvent.keyCode;
 
+  //console.log(typeof keyCodeOrEvent, keyCode);
+
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${keyCode}"]`);
 
@@ -19,14 +21,17 @@ function removeTransition(e) {
   e.target.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll(".key");
+const keys = document.querySelectorAll(".key"); // Div elements Node list
+//console.log(keys);
 
 keys.forEach((keyEl) => {
   keyEl.addEventListener("transitionend", removeTransition);
 
+  // **Parethesis for the function are allowed as the function is being passed via the arrow function
+  // **Arrow functions let you wrap a function call so it runs later.
   keyEl.addEventListener("click", () => {
     const keyCode = Number(keyEl.dataset.key); // "65" -> 65
-    playSound(keyCode);
+    playSound(keyCode); // **
   });
 });
 
